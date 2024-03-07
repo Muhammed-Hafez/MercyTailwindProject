090fd652f95a38663f1684c91f39ed979db3be69
Ali Samir <alisamir.eng@gmail.com>
2024-03-07 16:57:07 +0200

    dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html.js}"],
+  content: ["./index.html"],
   theme: {
     extend: {
-      keyframes:{
-        'open-menu' :{
-          '0%': {transform: 'scaleY(0)'},
-          '80%': {transform: 'scaleY(1.2)'},
-          '100%': {transform: 'scaleY(1)'},
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
         },
       },
-      animation:{
-        'open-menu': 'open-menu 10s ease-in-out forwards',
-      }
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
-}
-
+};
